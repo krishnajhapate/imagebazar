c71eb6cbe9ba01c24c23bfd2dfe3678823e39a3e
krishnajhapte <coronaprotection54@gmail.com>
2020-10-29 15:42:15 +0530

    first commit

--- imagebazar/urls.py
+++ imagebazar/urls.py
@@ -17,11 +17,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 import myapp
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myapp.urls'))
+    path('',include('myapp.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
